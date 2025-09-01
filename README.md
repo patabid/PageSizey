013d197db8c3d38a9788304099dfdda8a6891299
Melvin Newman <melvin@patabid.com>
2025-09-01 17:36:21 -0600

    First commit! Simple module with no dependancies (currently... no promises on the future). Provides a function page_size() to get the ANSI or ISO page size code for a given width and height.
    There is a basic test under the test folder that can be reviewed to see
    how to use the function and run to verify the output.
    
    The function returns a dictionary of values identify the standard and
    page size code.

---
+++ README.md
@@ -0,0 +1,29 @@
+# PageSizey
+## Sizing your paper
+
+PageSizey is a simple module written to facilitate getting the most likely paper page size from a set of input dimensions.
+
+Its primary use case is to identify the most likely page size code (ex. A4) from the `CropBox` feature in a pdf. 
+
+## Example:
+```
+from pagesizey import page_size
+
+size = page_size(8.5, 11) # inch page size in this case, mm values can also be used
+
+print(size)
+```
+
+## Returns
+```
+{
+    'standard': 'ansi',
+    'size': 'a',
+    'common_name': 'letter', # This only appears if there is a common name
+}
+```
+
+# License
+Simple, MIT, no warranty implied or given.
+
+Copywrite 2025 [PataBid Inc.](https://patabid.com)
---
+++ tests/test.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append("../src")
+
+from pagesizey import page_size
+
+print(f"[38;5;38mTEST 1: 24x36.[0m")
+size = page_size(24, 36)
+print(size)
+
+print(f"[38;5;38mTEST 2: 36x24.[0m")
+size = page_size(36, 24)
+print(size)
+
+print(f"[38;5;38mTEST 3: 36x22.[0m")
+size = page_size(34, 22)
+print(size)
+
+print(f"[38;5;38mTEST 4: 14.5x19.5.[0m")
+try:
+    size = page_size(14.5, 19.5)
+    print(size)
+except Exception as e:
+    print(f"[38;5;196m    {str(e)}[0m")
+
+print(f"[38;5;38mTEST 5: 8.5x11.[0m")
+size = page_size(8.5, 11)
+print(size)
+
+print(f"[38;5;38mTEST 6: 841x1189 (ISO).[0m")
+size = page_size(841, 1189)
+print(size)
+
+print(f"[38;5;38mTEST 7: 5.8x8.3 (Cursed ISO).[0m")
+size = page_size(5.8, 8.3)
+print(size)
+
+print(f"[38;5;38mTEST 8: 33.1x46.8 (Cursed ISO).[0m")
+size = page_size(33.1, 46.8)
+print(size)
+
+print(f"[38;5;38mTEST 9: 250x176 (ISO).[0m")
+size = page_size(250, 176)
+print(size)
